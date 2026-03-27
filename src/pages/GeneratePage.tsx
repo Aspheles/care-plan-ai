65b117c6b94862f7cb9147803a1d0a12886077e1
Yavuz <yavuzkaand@hotmail.com>
2026-03-27 01:23:40 +0100

    UI: proper styling when adding a new client

--- src/pages/GeneratePage.tsx
+++ src/pages/GeneratePage.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import useClientData from '../hooks/useClientData';
 import Loading from '../components/ui/Loading';
 import type { Client } from '../types/interface';
+import Button from '../components/ui/Button';
 
 export default function GeneratePage() {
   const [isSaving, setIsSaving] = useState(false);
@@ -16,64 +17,80 @@ export default function GeneratePage() {
     if (!currentClient) return;
     setIsSaving(true);
     setTimeout(() => {
-      // Call the api so the client can be saved into DB
       addClient(currentClient);
-      navigate('/');
+      navigate('/dashboard');
     }, 2000);
   };
 
   return (
     <div className='min-h-screen bg-gray-50 p-4'>
-      <button
-        onClick={() => navigate('/')}
-        className='mb-4 text-blue-600 cursor-pointer'
-      >
-        ← Terug
-      </button>
-      <h1 className='text-2xl font-bold mb-4'>Zorgplan</h1>
+      <div className='mx-auto'>
+        <button
+          onClick={() => navigate('/dashboard')}
+          className='mb-4 text-blue-600 cursor-pointer hover:underline'
+        >
+          ← Terug
+        </button>
 
-      {!isSaving && (
-        <>
-          <Section title='Client Volledige Naamt'>
-            <input
-              className={`w-full border rounded-xl p-2 `}
-              value={currentClient?.name || ''}
-              onChange={(e) =>
-                setCurrentClient({
-                  ...currentClient,
-                  id: Math.floor(Math.random() * 10000),
-                  name: e.target.value,
-                  status: 'Concept',
-                  plan: null,
-                } as Client)
-              }
-            />
-          </Section>
+        <h1 className='text-2xl font-bold'>Zorgplan</h1>
+        <p className='text-sm text-gray-500'>
+          Beheer cliëntgegevens en zorginformatie
+        </p>
 
-          <Section title='Client Geboorte Datum'>
-            <Editable
-              name='birthdate'
-              className='mt-2'
-              value=''
-              type='date'
-            ></Editable>
-          </Section>
+        <div className='bg-white rounded-2xl shadow-sm'>
+          {!isSaving && (
+            <>
+              <Section title='Algemene gegevens'>
+                <div>
+                  <label className='text-sm text-gray-500'>
+                    Volledige naam
+                  </label>
+                  <Editable
+                    name='name'
+                    className='w-full border border-gray-200 rounded-xl p-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                    value={currentClient?.name || ''}
+                    onChange={(e) =>
+                      setCurrentClient({
+                        ...currentClient,
+                        id:
+                          currentClient?.id ??
+                          Math.floor(Math.random() * 10000),
+                        name: e.target.value,
+                        status: 'Concept',
+                        plan: null,
+                      } as Client)
+                    }
+                  />
+                </div>
 
-          <button
-            onClick={saveClient}
-            className='w-full bg-blue-600 text-white p-4 mt-6 rounded-2xl text-lg cursor-pointer hover:bg-blue-500'
-          >
-            Opslaan
-          </button>
-        </>
-      )}
+                <div>
+                  <label className='text-sm text-gray-500'>Geboortedatum</label>
+                  <Editable
+                    name='birthdate'
+                    value=''
+                    type='date'
+                    className='w-full border border-gray-200 rounded-xl p-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                  />
+                </div>
+              </Section>
 
-      {isSaving && (
-        <div className='mt-10 text-center'>
-          <p className='text-lg'>Client wordt opgeslagen...</p>
-          <Loading />
+              <Button
+                onClick={saveClient}
+                className='w-full bg-blue-600 text-white p-4 mt-4 rounded-2xl text-lg cursor-pointer hover:bg-blue-500 transition'
+              >
+                Opslaan
+              </Button>
+            </>
+          )}
+
+          {isSaving && (
+            <div className='py-10 text-center'>
+              <p className='text-lg mb-2'>Client wordt opgeslagen...</p>
+              <Loading />
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
