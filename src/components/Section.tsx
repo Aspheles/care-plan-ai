2fb5d19301695cea6874758cf4369233c7fe090b
Yavuz <yavuzkaand@hotmail.com>
2026-03-27 01:22:52 +0100

    UI: added className property to section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,12 @@
 interface sectionProps {
-  title: string;
+  title?: string;
   children: React.ReactNode;
+  className?: string;
 }
 
-function Section({ title, children }: sectionProps) {
+function Section({ title, children, className }: sectionProps) {
   return (
-    <div className='bg-white p-4 rounded-2xl shadow mt-4'>
+    <div className={`bg-white p-4 rounded-2xl shadow mt-4 ${className}`}>
       <h2 className='font-semibold mb-2'>{title}</h2>
       {children}
     </div>
